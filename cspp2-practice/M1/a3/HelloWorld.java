85fe47be88e8e09e673878a5a97a161720173bd0
pvsteja <tejapathri6@msitprogram.net>
2018-08-27 18:37:59 +0530

    v

---
+++ cspp2-practice/M1/a3/HelloWorld.java
@@ -0,0 +1,23 @@
+/**
+ * HelloWorld.java.
+ * @author tejapathri
+ */
+public final class HelloWorld {
+    /**
+     * main function to print hello friends.
+     * Empty constructor
+     */
+    private HelloWorld() {
+
+    }
+    /**
+     *
+     * main function.
+     * @param args Not used variable.
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello friends!");
+        System.out.println("How are you ?");
+        System.out.println("Welcome to class 216-A");
+    }
+}
