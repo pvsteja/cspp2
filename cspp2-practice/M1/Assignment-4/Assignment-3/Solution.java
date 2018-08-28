f243170396820bd1003186a4ce2a5ec7b93259c0
pvsteja <tejapathri6@msitprogram.net>
2018-08-28 18:26:25 +0530

    v

--- cspp2-practice/M1/Assignment-4/Assignment-3/Solution.java
+++ cspp2-practice/M1/Assignment-4/Assignment-3/Solution.java
@@ -1,20 +1,31 @@
+/**
+ * Solution.java.
+ * @author tejapathri
+ */
  import java.util.Scanner;
 
-public class Solution {
-    /*
-    Do not modify this main function.
+public final class Solution {
+    /**
+    *Constructor.
+    */
+    private Solution() {
+    }
+    /**
+    *Do not modify this main function.
+    *@param args variable
     */
     public static void main(final String[] args) {
         Scanner scan = new Scanner(System.in);
         double degreesCelsius = scan.nextDouble();
         degreesCelsiustofahrenheit(degreesCelsius);
     }
-    /*
-    Need to fill the degreesCelsiustofahrenheit function and print the output
-    of fahrenheit.
+    /**
+    *Need to fill the degreesCelsiustofahrenheit function and print the output
+    *of fahrenheit.
+    * @param degreesCelsius variable
     */
     public static void degreesCelsiustofahrenheit(final double degreesCelsius) {
-        double fahrenheit = (degreesCelsius *9)/5 + 32;
+        final double fahrenheit = (degreesCelsius * 9) / 5 + 32;
         System.out.println(fahrenheit);
     }
 }
