ac9f1698641b311b71b75b8f853840367737adf1
pvsteja <tejapathri6@msitprogram.net>
2018-08-30 00:34:27 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-practice/M3/Assignment-2/Solution.java
+++ cspp2-practice/M3/Assignment-2/Solution.java
@@ -1,18 +1,18 @@
 
 import java.util.Scanner;
 /*
-	Do not modify this main function.
-	*/
+    Do not modify this main function.
+    */
 /**.
  * Class for solution.
  */
 public final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
 
-	}
+    }
 /* Fill the main function to print the number of 7's between 1 to n*/
     /**.
      * { function_description }
@@ -21,7 +21,7 @@ public final class Solution {
      */
     public static void main(final String[] args) {
 
-        Scanner s = new Scanner(System.in);      
+        Scanner s = new Scanner(System.in);
         final int n = s.nextInt();
         final int a = 10;
         final int b = 7;
@@ -38,18 +38,18 @@ public final class Solution {
      * @return     { description_of_the_return_value }
      */
     static int occurence(final int a, final int b, final int n) {
-    	int temp;
-    	int count = 0;
-    	for (int i = 1; i <= n; i++) {
-    		temp = i;
-    		while (temp != 0) {
-    			final int j = temp % a;
-    			if (j == b) {
-    				count += 1;
-    			}
-    			temp = temp / a;
-    		}
-    	}
-    	return count;
+        int temp;
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            temp = i;
+            while (temp != 0) {
+                final int j = temp % a;
+                if (j == b) {
+                    count += 1;
+                }
+                temp = temp / a;
+            }
+        }
+        return count;
     }
 }
