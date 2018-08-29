8b86a3125bdb6b80b5b7535ee839837ba406fee5
pvsteja <tejapathri6@msitprogram.net>
2018-08-29 23:21:01 +0530

    v

---
+++ cspp2-practice/M3/Activity-6/ConverttoWhileLoop.java
@@ -0,0 +1,25 @@
+/**.
+ * { item_description }
+ */
+public final class ConverttoWhileLoop {
+    /**
+     * Constructs the object.
+     */
+    private ConverttoWhileLoop() {
+
+    }
+    /**.
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        final i = 10;
+        for (int i = 2; i <= n;) {
+            System.out.println(i);
+            i = i + 2;
+        }
+        System.out.println("GoodBye");
+    }
+
+}
