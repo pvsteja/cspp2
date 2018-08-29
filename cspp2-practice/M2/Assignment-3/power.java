ffa024e25881bf3d66b4c582bfba799ca463675c
pvsteja <tejapathri6@msitprogram.net>
2018-08-29 20:24:11 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-practice/M2/Assignment-3/power.java
+++ cspp2-practice/M2/Assignment-3/power.java
@@ -5,19 +5,30 @@ import java.util.Scanner;
     */
 public class power {
 /* Fill the main function to print the number of 7's between 1 to n*/
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         Scanner scan = new Scanner(System.in);
-        long x = scan.nextInt();
-        long y = scan.nextInt();
+        final long x = scan.nextInt();
+        final long y = scan.nextInt();
         System.out.println(power(x, y));
     }
-        static long power(long x, long y){
-            if (y == 0)
+        /**
+         * 
+         *
+         * @param      x     { parameter_description }
+         * @param      y     { parameter_description }
+         *
+         * @return     { description_of_the_return_value }
+         */
+        static long power(final long x, final long y){
+            if (y == 0) {
                 return 1;
-            else if (y == 1)
+            }
+            else if (y == 1) {
                 return x;
-            else 
-                return (x * power(x, (y-1)));
+            }
+            else {
+                return (x * power(x, (y - 1)));
+            }
 
         }
 }
