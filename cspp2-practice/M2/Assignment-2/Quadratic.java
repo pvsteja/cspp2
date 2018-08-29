343f7dc16845f42b102259b502ad9ab9d454b079
pvsteja <tejapathri6@msitprogram.net>
2018-08-29 23:00:25 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-practice/M2/Assignment-2/Quadratic.java
+++ cspp2-practice/M2/Assignment-2/Quadratic.java
@@ -3,7 +3,7 @@ public class Quadratic {
   /*
   Do not modify this main function.
   */
-    public static void main(String[] args) {
+  public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
     final double a = scan.nextDouble();
     final double b = scan.nextDouble();
@@ -13,11 +13,12 @@ public class Quadratic {
   /*
   Need to write the rootsOfQuadraticEquation function and print the output.
   */
-      static void Quadratic(double a, double b, double c) {
-      double cons =  Math.sqrt(((b * b) - (4 * a * c)));
+    static void Quadratic(final double a, final double b, final double c) {
+
+      double cons =  Math.sqrt((b * b) - (4 * a *c));
       double r1 = (-1 * b + cons) / (2 * a);
       double r2 = (-1 * b - cons) / (2 * a);
       System.out.println(r1 + " " + r2);
-
     }
 }
+ 
