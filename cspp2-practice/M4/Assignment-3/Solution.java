c400135a1f8198d58a0243af4820742136328e5f
pvsteja <tejapathri6@msitprogram.net>
2018-08-31 17:09:42 +0530

    "check_style_score -> 1 of 1 passed. style score: 0/1 "

--- cspp2-practice/M4/Assignment-3/Solution.java
+++ cspp2-practice/M4/Assignment-3/Solution.java
@@ -3,34 +3,45 @@
 
 
 import java.util.Scanner;
+/**
+ * Class for solution.
+ */
 public class Solution {
-/*
-	Do not modify this main function.
-	*/
-	public static void main(final String[] args) {
-		Scanner sc=new Scanner(System.in);
-		int n= sc.nextInt();
-		for(int i=0;i<n;i++){
-			String s=sc.next();
-			int res=binaryToDecimal(s);//Write binaryToDecimal function
-			System.out.println(res);
-		}
-	}
-	/**
-	 * { function_description }
-	 *
-	 * @return     { description_of_the_return_value }
-	 */
-	public static int binaryToDecimal(final String s) {
-		int k = s.length() - 1;
-		int res = 0;
-		for (int i = 0; i <= s.length() - 1; i++) {
-			if (s.charAt(i) == '1') {
-				res += Math.pow(2, k);
-			}
-			k = k - 1;
-		}
-		return res;
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
 
-	}
+    }
+    /**
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 0; i < n; i++){
+            String s = sc.next();
+            int res = binaryToDecimal(s);//Write binaryToDecimal function
+            System.out.println(res);
+        }
+    }
+    /**
+     * { function_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public static int binaryToDecimal(final String s) {
+        int k = s.length() - 1;
+        int res = 0;
+        for (int i = 0; i <= s.length() - 1; i++) {
+            if (s.charAt(i) == '1') {
+                res += Math.pow(2, k);
+            }
+            k = k - 1;
+        }
+        return res;
+
+    }
 }
