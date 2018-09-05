8425a61052ee6ad0399d1e35ccdd1d9aa6e98258
pvsteja <tejapathri6@msitprogram.net>
2018-09-05 10:17:03 +0530

    "check_style_score -> 2 of 4 passed. style score: 0/1 "

--- cspp2-assignments/M9/Assignment-1.1/Assignment-1/Fibonacci.java
+++ cspp2-assignments/M9/Assignment-1.1/Assignment-1/Fibonacci.java
@@ -17,14 +17,29 @@ import java.util.Scanner;
  *
  */
 
-public class Fibonacci
-{
+/**.
+ * Class for fibonacci.
+ */
+public class Fibonacci {
+    /**
+     * Constructs the object.
+     */
+    private Fibonacci() {
+
+    }
     /*
      * fib is a static method takes a argument n
      * n is the count of the fibonacci numbers to be generated.
      * The method returns a List with the n fibonacci numbers.
      *
      * Look for the hint.txt if some of the testcases fail.
+     */
+    /**
+     * { function_description }
+     *
+     * @param      n     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
      */
     public static List fib(int n) {
         // todo - complete this method
@@ -44,7 +59,12 @@ public class Fibonacci
         return list;
     }
 
-	public static void main(String[] args) {
+    /**
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
         System.out.println(fib(n));
