13ec21735ab7093a7663235f968c05093b392437
pvsteja <tejapathri6@msitprogram.net>
2018-12-27 15:04:24 +0530

    "cspp2_m10_a1_1: 0/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- CSPP2 Remedial/CSPP2 Remedial Exam (1)/CSPP2 Remedial Exam/hotelBookings.java
+++ CSPP2 Remedial/CSPP2 Remedial Exam (1)/CSPP2 Remedial Exam/hotelBookings.java
@@ -1,11 +1,14 @@
+import java.util.Arrays;
 public class hotelBookings {
 	int size;
 	Reservation[] reservations;
 	int count=0;
+	int[] check;
 
 	public hotelBookings(){
 		size = 0;
 		reservations = new Reservation[5];
+		check = new int[5];
 
 	}
 	// public boolean isEmpty(int roomNum) {
@@ -25,6 +28,12 @@ public class hotelBookings {
 	 		return -1;
 		}
 		else {
+		 	for (int j = 0; j < check.length; j++) {
+		 		if (count == check[j]) {
+		 			count++;
+		 		}
+
+		 	}
 		 	reservations[count] = r;
 		 	++size;
 		 	count++;
@@ -35,12 +44,18 @@ public class hotelBookings {
 
 	public Boolean reserveRoom(String person, int roomNum) {
 		Reservation r = new Reservation(person,roomNum);
+		int i = 0;
+		check[i] = roomNum;
+		Arrays.sort(check);
+
 		// for (int i = 0; i < reservations.length; i++) {
 			if (reservations[roomNum - 1] == null) {
 				reservations[roomNum - 1] = r;
+				i++;
 				return true;
 			}
 		// }
+			i++;
 		return false;
 	}
 
