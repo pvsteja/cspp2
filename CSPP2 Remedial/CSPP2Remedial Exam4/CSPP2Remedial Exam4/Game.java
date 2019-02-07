3a55cab5008df54b419d68ca193a9e297338c06a
pvsteja <tejapathri6@msitprogram.net>
2019-02-07 11:56:29 +0530

    "cspp2_m10_a1_1: 0/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ CSPP2 Remedial/CSPP2Remedial Exam4/CSPP2Remedial Exam4/Game.java
@@ -0,0 +1,53 @@
+class Game{
+	int size;
+	Player[] player;
+	public Game(int count) {
+		player = new Player[count];
+		size = 0;
+	}
+	public void addPlayer(Player p){
+		if (size <= player.length) {
+			player[size] = p;
+			size++;
+		}
+	}
+	public void winner(){
+		for (int i = 0; i < player.length; i++) {
+			if (player[i].getScore() == 100) {
+				System.out.println(player[i].getName());
+			}
+		}
+	}
+	public int indexOf(String name){
+		for (int i = 0; i <= player.length; i++) {
+			if (player[i].getName().equals(name)) {
+				return 0;
+			}
+		}
+		return 1;
+	}
+}
+class Player{
+	private String name;
+	private int score;
+	public Player(String name){
+		this.name = name;
+		this.score = score;
+	}
+	public void setName(String name){
+		this.name = name;
+	}
+	public void setScore(int score){
+		this.score = score;
+	}
+	public int getScore(){
+		return score;
+	}
+	public String getName(){
+		return name;
+	}
+	public String toString() {
+		return this.name;
+	}
+
+}
---
+++ CSPP2 Remedial/CSPP2Remedial Exam4/CSPP2Remedial Exam4/Solution.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int count = Integer.parseInt(scan.nextLine());
+		Game game = new Game(count);
+		game.addPlayer(new Player(scan.nextLine()));
+		int totalScore = 0;
+		while (scan.hasNextLine()) {
+			String lines[] = scan.nextLine().split(" got ");
+			if (lines.length == 1) {
+				if (game.indexOf(lines[0]) == -1) {
+					game.addPlayer(new Player(lines[0]));
+				}
+			}  else if (lines.length == 2) {
+				String player = lines[0];
+				int index = game.indexOf(player);
+				int score = Integer.parseInt(lines[1]);
+				//Please complete the code to complete the game.
+				if (score == 1) {
+					totalScore += 0;
+				}else {
+					totalScore += score;
+				}
+			}
+		}
+		game.winner();
+	}
+}
